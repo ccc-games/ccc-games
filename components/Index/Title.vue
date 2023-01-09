b40be905cd369839851f43bf3d938d479ba271d6
Vincent WENDLING <vincent@starling-burgers.com>
2023-01-09 16:13:05 +0100

    initial commit

---
+++ components/Index/Title.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="title-wrapper">
+        <h1 id="ccc">Chilling Con Carne</h1>
+        <hr width="30">
+        <h1 id="games">Games</h1>           
+    </div>
+</template>
+
+<style scoped>
+
+#title-wrapper {
+    position: absolute;
+    top: 30%;
+    left: 50%;
+    transform: translate(-50%);
+
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+#ccc {
+    width: fit-content;
+    margin: auto;
+    color:white;
+    text-decoration: underline;
+}
+
+#games {
+    text-transform: uppercase;
+    font-size: 36px;
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <IndexTitle />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&display=swap");
+
+:root {
+  scroll-behavior: smooth;
+}
+
+body {
+  overflow-x: hidden;
+  margin: 0;
+  background-color: black;
+}
+
+html {
+  font-family: "Raleway", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
